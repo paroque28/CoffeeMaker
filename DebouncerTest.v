f97abe58ea178f01dad648db3b4537d53373db70
Pablo Rodriguez <paroque28@gmail.com>
2017-09-04 15:03:30 -0600

    add .gitignore

---
+++ DebouncerTest.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:50:37 09/04/2017 
+// Design Name: 
+// Module Name:    DebouncerTest 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module DebouncerTest(
+    input btnp,
+    output [7:3] led
+    );
+
+
+endmodule
