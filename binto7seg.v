ff83b964cbf78d6af83e16d8596fd58cf33e2545
Pablo Rodriguez <paroque28@gmail.com>
2017-09-14 13:38:02 -0600

    7 seg working

--- binto7seg.v
+++ binto7seg.v
@@ -19,43 +19,44 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module binto7seg(
-    input  [3:0]x,
-    output reg [6:0]seg
+	 input clk,
+    input  [3:0] binary,
+    output reg [6:0] seg
     );
-always @*
+always @(clk)
 case (binary)
 4'b0000 :      //Hexadecimal 0
-seg = 7'b1111110;
+seg = 7'b1000000;
 4'b0001 :    	//Hexadecimal 1
-seg = 7'b0110000  ;
+seg = 7'b1111001;
 4'b0010 :  		// Hexadecimal 2
-seg = 7'b1101101 ; 
+seg = 7'b0100100; 
 4'b0011 : 		// Hexadecimal 3
-seg = 7'b1111001 ;
+seg = 7'b0110000;
 4'b0100 :		// Hexadecimal 4
-seg = 7'b0110011 ;
+seg = 7'b0011001;
 4'b0101 :		// Hexadecimal 5
-seg = 7'b1011011 ;  
+seg = 7'b0010010;  
 4'b0110 :		// Hexadecimal 6
-seg = 7'b1011111 ;
+seg = 7'b0000010;
 4'b0111 :		// Hexadecimal 7
-seg = 7'b1110000;
+seg = 7'b1111000;
 4'b1000 :     	//Hexadecimal 8
-seg = 7'b1111111;
+seg = 7'b0000000;
 4'b1001 :    	//Hexadecimal 9
-seg = 7'b1111011 ;
+seg = 7'b0010000;
 4'b1010 :  		// Hexadecimal A
-seg = 7'b1110111 ; 
+seg = 7'b0001000; 
 4'b1011 : 		// Hexadecimal B
-seg = 7'b0011111;
+seg = 7'b0000011;
 4'b1100 :		// Hexadecimal C
-seg = 7'b1001110 ;
+seg = 7'b1000110;
 4'b1101 :		// Hexadecimal D
-seg = 7'b0111101 ;
+seg = 7'b0100001;
 4'b1110 :		// Hexadecimal E
-seg = 7'b1001111 ;
+seg = 7'b0000110;
 4'b1111 :		// Hexadecimal F
-seg = 7'b1000111 ;
+seg = 7'b0001110;
 endcase
  
 endmodule
